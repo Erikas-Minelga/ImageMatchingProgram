858c394af1ef3f71346ad355d318a938f6666efb
Erikas <erikas.minelga96@gmail.com>
2021-04-07 05:59:09 +0100

    Add project files.

---
+++ FileManager.h
@@ -0,0 +1,15 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+class FileManager
+{
+	public:
+		FileManager() {}
+		~FileManager() {}
+};
+
+#endif
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "FileManager.h"
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+
+	std::cin.ignore();
+
+	return 0;
+}
