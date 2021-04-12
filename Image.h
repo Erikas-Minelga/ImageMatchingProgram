11a9e93a168a0786fe191e8ff7f7e2a6cfcd949e
Erikas <erikas.minelga96@gmail.com>
2021-04-12 05:06:45 +0100

    Fixed Image.h as it was not merged into the main branch correctly

--- Image.h
+++ Image.h
@@ -1,32 +1,28 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
-#include <fstream>
-#include <sstream>
-#include <string>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 class Image
 {
 	public:
-		//Constructors
 		Image() {}
-		Image(std::string img_path);
 		Image(std::vector<int> data, int height, int width) { this->data = data; this->height = height; this->width = width; }
 		~Image() {}
+		Image(std::string img_path);
 		
-		//Methods
 		int sum();
 		void square();
 		Image createSubImage(int minWidth, int maxWidth, int minHeight, int maxHeight);
 		void drawOutline(int minWidth, int maxWidth, int minHeight, int maxHeight, int col);
 		void writeToFile(std::string path);
 
-		//Operator overloads
 		Image operator-(const Image& i);
 
-		//Getters
 		int Height() { return this->height; }
 		int Width() { return this->width; }
 		std::vector<int> Data() { return data; }
